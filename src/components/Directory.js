3ac5e199c7503b230f54aa8619fc373183453bad
Ben <46476247+hexagonatron@users.noreply.github.com>
2020-07-05 18:05:45 +0930

    filter working

--- src/components/Directory.js
+++ src/components/Directory.js
@@ -7,22 +7,47 @@ import EmployeeTable from './EmployeeTable'
 const Directory = () => {
 
     const [employees, setEmployees] = useState([]);
+    const [sortObj, updateSort] = useState({field: "name", order: "dsc"});
+    const [filterObj, updateFilter] = useState({name:""});
+    const [filteredEmployees, updateFilteredEmployees] = useState([]);
 
+    //Get some random employees on page load
     useEffect(() => {
         fetch('https://randomuser.me/api?results=50').then(res => res.json()).then(json => {
             console.log(json)
             setEmployees(json.results);
         })
-
-
     }, []);
+
+    //Update filtered employees whenever something changes
+    useEffect(() => {
+        console.log(filterObj);
+        
+        let newEmployees = employees
+
+        if(newEmployees.length && filterObj.name){
+            newEmployees = newEmployees.filter(employee => {
+                return (employee.name.first.toLowerCase().includes(filterObj.name.toLowerCase()));
+            })
+        }
+
+        if(newEmployees.length && sortObj){
+            newEmployees = newEmployees.filter(employee => {
+                return (employee.name.first.toLowerCase().includes(filterObj.name.toLowerCase()));
+            })
+        }
+
+        updateFilteredEmployees(newEmployees);
+    }, [employees, sortObj, filterObj])
+
+
 
 
 
     return (
         <div className="container">
-            <DirectoryControls />
-            <EmployeeTable employees={employees}/>
+            <DirectoryControls updateFilter={updateFilter}/>
+            <EmployeeTable employees={filteredEmployees} />
         </div>
     );
 };
--- src/components/DirectoryControls.js
+++ src/components/DirectoryControls.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
-const DirectoryControls = () => {
+const DirectoryControls = ({updateFilter}) => {
+
+    const filterChangeHandler = (e) => {
+        updateFilter({name: e.target.value})
+    }
+
     return (
         <div>
-            
+            <input type="text" className="form-control" placeholder="Search by name" onChange={filterChangeHandler}></input>
         </div>
     );
 };
